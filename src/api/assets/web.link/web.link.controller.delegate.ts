4c4f34c9c29349cd7fbc8dd6c06aacf14324d28d
MadhuraAnarase <madhuraa712@gmail.com>
2025-06-06 22:10:54 +0530

    Added TenantId in Web link

--- src/api/assets/web.link/web.link.controller.delegate.ts
+++ src/api/assets/web.link/web.link.controller.delegate.ts
@@ -127,6 +127,10 @@ export class WebLinkControllerDelegate {
         if (version != null) {
             filters['Version'] = version;
         }
+        var tenantId = query.tenantId ? query.tenantId : null;
+        if (tenantId != null) {
+            filters['TenantId'] = tenantId;
+        }
 
         return filters;
     };
@@ -153,6 +157,9 @@ export class WebLinkControllerDelegate {
         if (Helper.hasProperty(requestBody, 'Version')) {
             updateModel.Version = requestBody.Version;
         }
+        if (Helper.hasProperty(requestBody, 'TenantId')) {
+            updateModel.TenantId = requestBody.TenantId;
+        }
 
         return updateModel;
     };
@@ -165,7 +172,8 @@ export class WebLinkControllerDelegate {
             Url         : requestBody.Url ? requestBody.Url : null,
             Tags        : requestBody.Tags ? JSON.stringify(requestBody.Tags) as string : JSON.stringify([]),
             Version     : requestBody.Version ? requestBody.Version : 'V1',
-            OwnerUserId : requestBody.OwnerUserId
+            OwnerUserId : requestBody.OwnerUserId,
+            TenantId    : requestBody.TenantId ? requestBody.TenantId : null,
         };
     };
 
@@ -181,6 +189,7 @@ export class WebLinkControllerDelegate {
             Url           : record.Url,
             AssetCategory : record.AssetCategory,
             OwnerUserId   : record.OwnerUserId,
+            TenantId      : record.TenantId,
             Tags          : JSON.parse(record.Tags),
             Version       : record.Version
         };
@@ -198,6 +207,7 @@ export class WebLinkControllerDelegate {
             Url           : record.Url,
             AssetCategory : record.AssetCategory,
             OwnerUserId   : record.OwnerUserId,
+            TenantId      : record.TenantId,
             Tags          : JSON.parse(record.Tags),
             Version       : record.Version,
             CreatedAt     : record.CreatedAt,
