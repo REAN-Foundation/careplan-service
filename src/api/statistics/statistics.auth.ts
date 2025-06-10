430f3a3515bd63276339b0eacb3853bc0dd0ac91
RupaliD <rupali.dinde@inflectionzone.com>
2025-06-10 15:32:03 +0530

    Updated the premission files

--- src/api/statistics/statistics.auth.ts
+++ src/api/statistics/statistics.auth.ts
@@ -10,15 +10,14 @@ import {
 
 export class StatisticsAuth {
 
-    static readonly _baseContext = `Statistcs`;
+    static readonly _baseContext = `Statistics`;
 
     static readonly search: AuthOptions = {
         ...DefaultAuthOptions,
-        Context             : `${this._baseContext}.Search`,
-        Ownership           : ResourceOwnership.System,
-        ActionScope         : ActionScope.Public,
-        RequestType         : RequestType.Search,
-        CustomAuthorization : true,
+        Context     : `${this._baseContext}.Search`,
+        Ownership   : ResourceOwnership.System,
+        ActionScope : ActionScope.Public,
+        RequestType : RequestType.Search,
     };
 
 }
